dc51868785c9d409664de36502138bd4403fc87d
owl <dmitrikojevnikov@yandex.ru>
2026-07-07 12:17:01 +0300

    Создать архитектуру и поднять сервисы

---
+++ statistics-service/statistics-service-server/src/main/java/ru/practicum/statistics/server/StatisticsServerApplication.java
@@ -0,0 +1,11 @@
+package ru.practicum.statistics.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsServerApplication.class);
+    }
+}
