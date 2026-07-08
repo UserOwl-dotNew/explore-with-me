ff277fad7f7922f4f4073b68bf2c4ce492353c49
owl <dmitrikojevnikov@yandex.ru>
2026-07-08 20:43:38 +0300

    Удалить неиспользуемые импорты

--- statistics-service/statistics-service-server/src/test/java/ru/practicum/statistics/server/service/StatsServiceImplTest.java
+++ statistics-service/statistics-service-server/src/test/java/ru/practicum/statistics/server/service/StatsServiceImplTest.java
@@ -14,7 +14,8 @@ import ru.practicum.statistics.server.repository.StatsRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
