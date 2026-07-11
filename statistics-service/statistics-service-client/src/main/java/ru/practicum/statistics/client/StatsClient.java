7db09f7ac122fce2e9391106a924b378ac01efb4
Anna <mussina1anna@yandex.ru>
2026-07-11 12:03:34 +0300

    feat: реализован HTTP-клиент статистики

---
+++ statistics-service/statistics-service-client/src/main/java/ru/practicum/statistics/client/StatsClient.java
@@ -0,0 +1,54 @@
+package ru.practicum.statistics.client;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.practicum.statistics.dto.EndpointHitDto;
+import ru.practicum.statistics.dto.ViewStatsDto;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StatsClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${stats-server.url:http://localhost:9090}")
+    private String serverUrl;
+
+    public void sendHit(EndpointHitDto hit) {
+        try {
+            restTemplate.postForEntity(serverUrl + "/hit", hit, Void.class);
+            log.info("Отправлен хит: {}", hit);
+        } catch (Exception e) {
+            log.error("Ошибка при отправке хита: {}", e.getMessage());
+        }
+    }
+
+    public List<ViewStatsDto> getStats(String start, String end, List<String> uris, Boolean unique) {
+        try {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUrl + "/stats")
+                    .queryParam("start", start)
+                    .queryParam("end", end);
+            if (uris != null && !uris.isEmpty()) {
+                builder.queryParam("uris", String.join(",", uris));
+            }
+            if (unique != null) {
+                builder.queryParam("unique", unique);
+            }
+
+            ViewStatsDto[] response = restTemplate.getForObject(builder.toUriString(), ViewStatsDto[].class);
+            return Arrays.asList(response);
+        } catch (Exception e) {
+            log.error("Ошибка при получении статистики: {}", e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+}
