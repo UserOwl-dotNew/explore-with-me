f9d3a46efe93fe27670940058070764033be8776
Anna <mussina1anna@yandex.ru>
2026-07-24 08:14:09 +0300

    fix: еще строки)

--- main-service/src/main/java/ru/practicum/mainservice/repository/ParticipationRequestRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/repository/ParticipationRequestRepository.java
@@ -9,9 +9,14 @@ import java.util.Optional;
 
 public interface ParticipationRequestRepository extends JpaRepository<ParticipationRequest, Long> {
     List<ParticipationRequest> findByRequesterId(Long userId);
+
     Optional<ParticipationRequest> findByEventIdAndRequesterId(Long eventId, Long userId);
+
     List<ParticipationRequest> findByEventId(Long eventId);
+
     long countByEventIdAndStatus(Long eventId, RequestStatus status);
+
     List<ParticipationRequest> findByEventIdAndIdIn(Long eventId, List<Long> ids);
+
     List<ParticipationRequest> findByEventIdAndStatus(Long eventId, RequestStatus status);
 }
