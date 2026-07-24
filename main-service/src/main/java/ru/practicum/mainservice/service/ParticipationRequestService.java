29b06216cbca8eac7547004d407136f26a9d126e
Anna <mussina1anna@yandex.ru>
2026-07-24 08:10:12 +0300

    fix: исправлены ошибки Checkstyle (пустые строки, импорты)

--- main-service/src/main/java/ru/practicum/mainservice/service/ParticipationRequestService.java
+++ main-service/src/main/java/ru/practicum/mainservice/service/ParticipationRequestService.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 public interface ParticipationRequestService {
     List<ParticipationRequestDto> getUserRequests(Long userId);
+
     ParticipationRequestDto addRequest(Long userId, Long eventId);
+
     ParticipationRequestDto cancelRequest(Long userId, Long requestId);
+
     List<ParticipationRequestDto> getEventRequests(Long userId, Long eventId);
+
     EventRequestStatusUpdateResult updateRequestStatus(Long userId, Long eventId,
                                                        EventRequestStatusUpdateRequest updateRequest);
 }
