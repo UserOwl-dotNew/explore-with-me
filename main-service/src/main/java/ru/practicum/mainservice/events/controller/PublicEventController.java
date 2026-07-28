6618e46cff86617c4637a91178bad88f8720d045
Artem Ledovskikh <ledartem@bk.ru>
2026-07-28 20:26:22 +0700

    Fix checkstyle

--- main-service/src/main/java/ru/practicum/mainservice/events/controller/PublicEventController.java
+++ main-service/src/main/java/ru/practicum/mainservice/events/controller/PublicEventController.java
@@ -1,6 +1,5 @@
 package ru.practicum.mainservice.events.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
