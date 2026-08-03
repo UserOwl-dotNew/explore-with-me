8b07cf60c49995dd40f09a407684d0bc2c897706
Artem Ledovskikh <ledartem@bk.ru>
2026-08-04 00:29:03 +0700

    Реализация сервисов и репозитория

--- main-service/src/main/java/ru/practicum/mainservice/comments/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/comments/repository/CommentRepository.java
@@ -1,9 +1,58 @@
 package ru.practicum.mainservice.comments.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.mainservice.comments.entity.Comment;
 
-@Repository
+import java.util.Optional;
+
+/**
+ * Репозиторий для работы с комментариями.
+ */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    /**
+     * Получение неудалённых комментариев события.
+     *
+     * @param eventId идентификатор события
+     * @param pageable параметры пагинации и сортировки
+     * @return страница неудалённых комментариев
+     */
+    Page<Comment> findAllByEventIdAndIsDeletedFalse(
+            Long eventId,
+            Pageable pageable
+    );
+
+    /**
+     * Получение неудалённого комментария по идентификатору.
+     *
+     * @param commentId идентификатор комментария
+     * @return найденный неудалённый комментарий
+     */
+    Optional<Comment> findByIdAndIsDeletedFalse(Long commentId);
+
+    /**
+     * Получение всех комментариев пользователя, включая удалённые.
+     *
+     * @param authorId идентификатор автора
+     * @param pageable параметры пагинации и сортировки
+     * @return страница комментариев пользователя
+     */
+    Page<Comment> findAllByAuthorId(
+            Long authorId,
+            Pageable pageable
+    );
+
+    /**
+     * Получение всех комментариев события, включая удалённые.
+     *
+     * @param eventId идентификатор события
+     * @param pageable параметры пагинации и сортировки
+     * @return страница комментариев события
+     */
+    Page<Comment> findAllByEventId(
+            Long eventId,
+            Pageable pageable
+    );
 }
