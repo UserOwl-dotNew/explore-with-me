aeff5253391e75e590d91889fd4457aeb6b9a837
Anna <mussina1anna@yandex.ru>
2026-08-06 20:18:25 +0300

    fix: исправлен StatsClient и поле deleted [skip ci]

--- main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
@@ -3,11 +3,13 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"ru.practicum.common.entity", "ru.practicum.mainservice"})
 @EnableJpaRepositories(basePackages = "ru.practicum.mainservice")
+@ComponentScan({"ru.practicum.mainservice", "ru.practicum.statistics.client"})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
