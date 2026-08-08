14eb0113a5814c8fbaa423e5f729ccf7a9827d5c
owl <dmitrikojevnikov@yandex.ru>
2026-08-08 20:19:40 +0300

    fix: rollback в последнюю рабочую версию

--- main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
@@ -3,15 +3,18 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "ru.practicum")
-@EntityScan(basePackages = {
-        "ru.practicum.common",
-        "ru.practicum.mainservice"
-})
+@SpringBootApplication
+@EntityScan(basePackages = {"ru.practicum.common.entity", "ru.practicum.mainservice"})
+@EnableJpaRepositories(basePackages = "ru.practicum.mainservice")
+@ComponentScan({"ru.practicum.mainservice", "ru.practicum.statistics.client"})
 public class MainServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class);
+        SpringApplication.run(MainServiceApplication.class, args);
     }
+
 }
 
--- main-service/src/main/java/ru/practicum/mainservice/comments/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/comments/repository/CommentRepository.java
@@ -19,10 +19,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @param pageable параметры пагинации и сортировки
      * @return страница неудалённых комментариев
      */
-    Page<Comment> findAllByEventIdAndIsDeletedFalse(
-            Long eventId,
-            Pageable pageable
-    );
+    Page<Comment> findByEventIdAndDeletedFalse(Long eventId, Pageable pageable);
 
     /**
      * Получение неудалённого комментария по идентификатору.
@@ -30,7 +27,24 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @param commentId идентификатор комментария
      * @return найденный неудалённый комментарий
      */
-    Optional<Comment> findByIdAndIsDeletedFalse(Long commentId);
+    Optional<Comment> findByIdAndDeletedFalse(Long commentId);
+
+    /**
+     * Проверка существования неудалённого комментария по идентификатору.
+     *
+     * @param commentId идентификатор комментария
+     * @return true, если неудалённый комментарий с таким id существует
+     */
+    boolean existsByIdAndDeletedFalse(Long commentId);
+
+    /**
+     * Проверка существования неудалённого комментария у автора.
+     *
+     * @param commentId идентификатор комментария
+     * @param authorId  идентификатор автора
+     * @return true, если комментарий принадлежит автору и не удалён
+     */
+    boolean existsByIdAndAuthorIdAndDeletedFalse(Long commentId, Long authorId);
 
     /**
      * Получение всех комментариев пользователя, включая удалённые.
@@ -39,10 +53,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @param pageable параметры пагинации и сортировки
      * @return страница комментариев пользователя
      */
-    Page<Comment> findAllByAuthorId(
-            Long authorId,
-            Pageable pageable
-    );
+    Page<Comment> findAllByAuthorId(Long authorId, Pageable pageable);
 
     /**
      * Получение всех комментариев события, включая удалённые.
@@ -51,8 +62,20 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @param pageable параметры пагинации и сортировки
      * @return страница комментариев события
      */
-    Page<Comment> findAllByEventId(
-            Long eventId,
-            Pageable pageable
-    );
+    Page<Comment> findAllByEventId(Long eventId, Pageable pageable);
+
+    /**
+     * Удаление всех комментариев автора (например, при удалении пользователя).
+     *
+     * @param authorId идентификатор автора
+     */
+    void deleteAllByAuthorId(Long authorId);
+
+    /**
+     * Получение количества неудалённых комментариев события.
+     *
+     * @param eventId идентификатор события
+     * @return количество неудалённых комментариев
+     */
+    long countByEventIdAndDeletedFalse(Long eventId);
 }
