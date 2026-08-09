ccc58e6b1dae399d40ca2220238220dff70c46ca
owl <dmitrikojevnikov@yandex.ru>
2026-08-09 16:53:45 +0300

    fix: изменить область сканирования для класса MainServiceApplication

--- main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/ru/practicum/mainservice/MainServiceApplication.java
@@ -3,13 +3,12 @@ package ru.practicum.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan(basePackages = {"ru.practicum.common.entity", "ru.practicum.mainservice"})
-@EnableJpaRepositories(basePackages = "ru.practicum.mainservice")
-@ComponentScan({"ru.practicum.mainservice", "ru.practicum.statistics.client"})
+@SpringBootApplication(scanBasePackages = "ru.practicum")
+@EntityScan(basePackages = {
+        "ru.practicum.common",
+        "ru.practicum.mainservice"
+})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
@@ -17,4 +16,3 @@ public class MainServiceApplication {
     }
 
 }
-
